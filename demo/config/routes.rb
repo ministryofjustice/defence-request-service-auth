4bf2591b1fc39e6f5d70e8616bdc606149bfdbeb
Chris Carter <chris.carter@unboxedconsulting.com>
2015-07-15 14:36:52 +0100

    Fix Rubocop issues

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root "welcome#index"
 
   get "/auth/:provider/callback", to: "sessions#create"
   get "/auth/failure", to: "sessions#failure"
